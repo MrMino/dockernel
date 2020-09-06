d8c85442563298cb9aef01da1bd608a2cf78ad5b
Blazej Michalik <blazej.michalik@nokia.com>
2020-09-07 01:43:53 +0200

    Implement start subcommand

--- dockernel/cli/start.py
+++ dockernel/cli/start.py
@@ -1,4 +1,7 @@
-from .main import subparsers
+import docker
+from .main import subparsers, set_subcommand_func
+from argparse import Namespace
+from pathlib import Path
 
 
 arguments = subparsers.add_parser(
@@ -20,3 +23,48 @@ arguments.add_argument(
     'connection_file',
     help="The connection file to use."
 )
+
+
+CONTAINER_CONNECTION_SPEC_PATH = '/kernel-connection-spec.json'
+CONTAINER_CONNECTION_SPEC_ENV_VAR = 'DOCKERNEL_CONNECTION_FILE'
+
+
+def start(parsed_args: Namespace) -> int:
+    containers = docker.from_env().containers
+    image_name = parsed_args.image_name
+    connection_file = Path(parsed_args.connection_file)
+
+    # TODO: parametrize connection spec file bind path
+    connection_file_mount = docker.types.Mount(
+        target=CONTAINER_CONNECTION_SPEC_PATH,
+        source=str(connection_file.absolute()),
+        type='bind',
+        # XXX: some kernels still open connection spec in write mode
+        # (I'm looking at you, IPython), even though it's not being written
+        # into.
+        read_only=False
+    )
+
+    env_vars = {
+        CONTAINER_CONNECTION_SPEC_ENV_VAR: CONTAINER_CONNECTION_SPEC_PATH
+    }
+
+    # TODO: network_mode shouldn't be "host", expose necessary ports only
+    # TODO: parametrize possible mounts
+    # TODO: log stdout and stderr
+    # TODO: use detached=True?
+    containers.run(
+        image_name,
+        auto_remove=True,
+        environment=env_vars,
+        mounts=[connection_file_mount],
+        network_mode='host',
+        stdout=True,
+        stderr=True
+    )
+
+    # TODO: bare numbered exit statusses seem bad
+    return 0
+
+
+set_subcommand_func(parser=arguments, func=start)
