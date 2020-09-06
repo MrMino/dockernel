496df55bd37d80adf500f7100c4307fa08dafee9
Blazej Michalik <blazej.michalik@nokia.com>
2020-09-06 05:01:27 +0200

    Add the rest of kernelspec.py its and tests

--- dockernel/kernelspec.py
+++ dockernel/kernelspec.py
@@ -1,6 +1,19 @@
+"""Kernelspec installation facilities
+
+Where term "kernelspec dir" is used, it references the directory in which
+kernel.json file is present.
+Where term "kernelspec store" is used, it references the directory where
+jupyter will look for kernelspec dirs.
+"""
+import json
+import string
 from typing import List, Dict, Optional
 from enum import Enum
-import json
+from pathlib import Path
+
+
+KERNELSPEC_FILENAME = 'kernel.json'
+KERNELSPEC_STORE_DIRNAME = 'kernels'
 
 
 class InterruptMode(str, Enum):
@@ -9,7 +22,7 @@ class InterruptMode(str, Enum):
 
 
 class Kernelspec:
-    """ Represents generic kernelspec for a kernel
+    """Represents generic kernelspec for a kernel
 
     See jupyter-client docs on kernelspec files for explanation of arguments
     taken by the __init__ of this class.
@@ -22,17 +35,131 @@ class Kernelspec:
                  env: Optional[str] = None,
                  metadata: Optional[Dict[str, str]] = None
                  ) -> None:
-        self.spec = {}
-        self.spec['argv'] = argv
-        self.spec['display_name'] = display_name
-        self.spec['language'] = language
+        self._spec = {}
+        self._spec['argv'] = argv
+        self._spec['display_name'] = display_name
+        self._spec['language'] = language
 
         if interrupt_mode is not None:
-            self.spec['interrupt_mode'] = interrupt_mode
+            self._spec['interrupt_mode'] = interrupt_mode
         if env is not None:
-            self.spec['env'] = env
+            self._spec['env'] = env
         if metadata is not None:
-            self.spec['metadata'] = metadata
+            self._spec['metadata'] = metadata
 
-    def __str__(self):
-        return json.dumps(self.spec)
+    def json(self):
+        return json.dumps(self._spec)
+
+
+def user_kernelspec_store(system_type: str) -> Path:
+    """Return path to the place where user's kernelspecs are stored on given OS.
+
+    Parameters
+    ----------
+    system_type
+        Output of the builtin ``platform.system()``.
+    kernel_id
+        Internal identifier of the kernel to instal. Should be short and
+        URL-friendly. Should contain only ASCII numbers, ASCII letters, hyphen,
+        period, underscore.
+
+    Raises
+    ------
+    ValueError
+        If `system_type` is not one of the supported types.
+
+    Returns
+    -------
+    Path
+        Path object to the per-user directory where kernelspec dirs are stored.
+    """
+
+    if system_type == "Linux":
+        kernelspec_dir_path = "~/.local/share/jupyter/kernels"
+    elif system_type == "Windows":
+        kernelspec_dir_path = r"%APPDATA%\jupyter\kernels"
+    elif system_type == "Darwin":
+        kernelspec_dir_path = "~/Library/Jupyter/kernels"
+    else:
+        raise ValueError(f'unknown system type: {system_type}')
+
+    return Path(kernelspec_dir_path)
+
+
+def kernelspec_dir(kernelspec_store: Path, kernel_id: str) -> Path:
+    """Return path to the kernelspec directory for a kernel with a given ID
+
+    Parameters
+    ----------
+    kernelspec_store
+        Path to the place kernelspec store where kernelspec dir should be
+        placed.
+    kernel_id
+        Internal identifier of the kernel to instal. Should be short and
+        URL-friendly. Must contain only ASCII numbers, ASCII letters, hyphen,
+        period, underscore.
+
+    Raises
+    ------
+    ValueError
+        If `kernel_id` contains improper characters.
+
+    Returns
+    -------
+    Path
+        Path object to the directory where kernelspec should be installed.
+    """
+    allowed_characters = set(string.ascii_letters + string.digits + '_.-')
+    if not set(kernel_id) <= allowed_characters:
+        raise ValueError("kernel_id contains forbidden characters")
+
+    return kernelspec_store / kernel_id
+
+
+def ensure_kernelspec_store_exists(kernelspec_store: Path) -> None:
+    """Creates the kernel store directory if one doesn't exist.
+
+    Parameters
+    ----------
+    kernelspec_store
+        Path object to the store. The directory must be named "kernels".
+
+    Raises
+    ------
+    ValueError
+        If given path is not a valid store path, i.e. it doesn't end with
+        "kernels".
+    """
+    if kernelspec_store.name != KERNELSPEC_STORE_DIRNAME:
+        raise ValueError("not a valid kernelspec store name: "
+                         f"{repr(kernelspec_store.name)} "
+                         "- should be 'kernels'.")
+    if not kernelspec_store.exists():
+        kernelspec_store.mkdir()
+
+
+def install_kernelspec(kernelspec_dir: Path, kernelspec: Kernelspec) -> None:
+    """Generate appropriate kernelspec under the specified path.
+
+    Creates a directory for the kernelspec and populates it with the spec
+    files.
+
+    Parameters
+    ----------
+    kernelspec_dir
+        Path object to the store where the kernel should be installed.
+    kernelspec:
+        Kernelspec to render.
+
+    Raises
+    ------
+    ValueError
+        If a kernelspec already exists, i.e. the kernelspec directory is
+        present.
+    """
+    if kernelspec_dir.exists():
+        raise ValueError(f"kernelspec already exists: {kernelspec_dir}.")
+
+    kernelspec_dir.mkdir()
+    kernelspec_file = kernelspec_dir/KERNELSPEC_FILENAME
+    kernelspec_file.write_text(kernelspec.json())
--- tests/test_kernelspec.py
+++ tests/test_kernelspec.py
@@ -1,42 +1,142 @@
-from dockernel.kernelspec import Kernelspec, InterruptMode
-import json
 import pytest
+import json
+from dockernel.kernelspec import (Kernelspec, InterruptMode,
+                                  user_kernelspec_store, kernelspec_dir,
+                                  ensure_kernelspec_store_exists,
+                                  install_kernelspec)
+from pathlib import Path
 
 
-def test_InterruptMode_has_proper_values():
-    for mode in InterruptMode:
-        assert mode.name == mode.value
+@pytest.fixture
+def tmpdir(tmpdir):
+    """Workaround for tmpdir being py.path.LocalPath instead of Path"""
+    return Path(str(tmpdir))
+
+
+@pytest.fixture
+def kernelspec_dict():
+    return {
+        'argv': ['some', 'argument', 'vector'],
+        'language': 'klingon',
+        'display_name': 'test kernel',
+        'interrupt_mode': 'signal',
+        'env': {'var-a': 'val-a', 'var-b': 'val-b'},
+        'metadata': {'meta-a': 'meta-val-a', 'meta-b': 'meta-val-b'}
+    }
+
+
+@pytest.fixture
+def kernelspec(kernelspec_dict):
+    return Kernelspec(**kernelspec_dict)
+
+
+class TestInterruptMode:
+    @pytest.mark.parametrize('enum', InterruptMode)
+    def test_values_are_same_as_names(self, enum):
+        assert enum.name == enum.value
+
+    @pytest.mark.parametrize('enum', InterruptMode)
+    def test_values_are_strings(self, enum):
+        assert isinstance(enum.value, str)
 
 
 class TestKernelspec:
-    def test_kernelspec(self):
-        data_given = {
-            'argv': ['some', 'argument', 'vector'],
-            'language': 'klingon',
-            'display_name': 'test kernel',
-            'interrupt_mode': 'signal',
-            'env': {'var-a': 'val-a', 'var-b': 'val-b'},
-            'metadata': {'meta-a': 'meta-val-a', 'meta-b': 'meta-val-b'}
-        }
+    def test_kernelspec_renders_json_dict(self, kernelspec, kernelspec_dict):
+        dict_from_kernelspec = json.loads(kernelspec.json())
+        assert dict_from_kernelspec == kernelspec_dict
 
-        data_from_kernelspec = json.loads(str(Kernelspec(**data_given)))
+    @pytest.mark.parametrize('interrupt_mode', InterruptMode)
+    def test_InterruptMode_enum_is_casted(self, interrupt_mode,
+                                          kernelspec_dict):
+        del kernelspec_dict['interrupt_mode']
+        kernelspec = Kernelspec(interrupt_mode=interrupt_mode,
+                                **kernelspec_dict)
 
-        assert data_given == data_from_kernelspec
+        received_value = json.loads(kernelspec.json())['interrupt_mode']
 
-    @pytest.mark.parametrize('interrupt_mode_enum', InterruptMode)
-    def test_InterruptMode_enum_is_casted(self, interrupt_mode_enum):
-        mandatory_args = {
-            'argv': [], 'language': '', 'display_name': '',
-        }
+        assert interrupt_mode.value == received_value
 
-        kernelspec = Kernelspec(interrupt_mode=interrupt_mode_enum,
-                                **mandatory_args)
-        received_value = json.loads(str(kernelspec))['interrupt_mode']
+    def test_optional_args_are_optional(self):
+        Kernelspec(argv=['cmd'], language='', display_name='')
 
-        assert interrupt_mode_enum.value == received_value
 
-    def test_optional_args(self):
-        only_mandatory_args = {
-            'argv': [], 'language': '', 'display_name': '',
-        }
-        Kernelspec(**only_mandatory_args)
+class TestEnsureKernelspecStoreExists:
+    @pytest.fixture
+    def store(self, tmpdir):
+        return tmpdir/'kernels'
+
+    def test_store_dir_is_created_if_absent(self, store):
+        assert not store.exists()
+        ensure_kernelspec_store_exists(store)
+        assert store.is_dir()
+
+    def test_store_dir_is_not_created_if_present(self, store):
+        store.mkdir()
+        ensure_kernelspec_store_exists(store)
+
+    def test_raises_ValueError_when_directory_name_is_not_store(self, tmpdir):
+        with pytest.raises(ValueError):
+            ensure_kernelspec_store_exists(tmpdir/'system32')
+
+
+class TestKernelspecStoreDir:
+    @pytest.mark.parametrize('system_type', ('Linux', 'Windows', 'Darwin'))
+    def test_works_for_supported_platforms(self, system_type):
+        user_kernelspec_store(system_type)
+
+    def test_raises_TypeError_on_unknown_system(self):
+        with pytest.raises(ValueError):
+            user_kernelspec_store('unknown system type')
+
+
+class TestKernelspecDir:
+    def test_kernel_id_is_dir_name(self):
+        store = Path('/path/to/kernels/')
+        kernel_id = 'my-awesome-kernel'
+        returned_path = kernelspec_dir(store, kernel_id)
+
+        assert returned_path == store/kernel_id
+
+    @pytest.mark.parametrize('bad_id', ('{bad}', '<very></bad>', '=+!~'))
+    def test_raises_ValueError_when_given_bad_id(self, bad_id):
+        with pytest.raises(ValueError):
+            kernelspec_dir(Path(), bad_id)
+
+
+class TestInstallKernelspec:
+    @pytest.fixture
+    def kernelspec_dir(self, tmpdir):
+        return tmpdir/'my-kernel'
+
+    @pytest.fixture
+    def kernelspec_file(self, kernelspec_dir):
+        return kernelspec_dir/'kernel.json'
+
+    def test_raises_ValueError_if_kernelspec_exists(self, kernelspec_dir,
+                                                    kernelspec):
+        kernelspec_dir.mkdir()
+        with pytest.raises(ValueError):
+            install_kernelspec(kernelspec_dir, kernelspec)
+
+    def test_creates_kernelspec_directory(self, kernelspec_dir, kernelspec):
+        assert not kernelspec_dir.exists()
+        install_kernelspec(kernelspec_dir, kernelspec)
+        assert kernelspec_dir.is_dir()
+
+    def test_creates_kernelspec_file(self, kernelspec_dir, kernelspec,
+                                     kernelspec_file):
+        assert not kernelspec_file.exists()
+        install_kernelspec(kernelspec_dir, kernelspec)
+        assert kernelspec_file.is_file()
+
+    def test_populates_kernelspec_file_with_valid_json(self, kernelspec_dir,
+                                                       kernelspec_file,
+                                                       kernelspec):
+        install_kernelspec(kernelspec_dir, kernelspec)
+        json.loads(kernelspec_file.read_text())
+
+    def test_populates_file_with_kernelspec_data(self, kernelspec_dir,
+                                                 kernelspec_file, kernelspec):
+        install_kernelspec(kernelspec_dir, kernelspec)
+        text_contents = kernelspec_file.read_text()
+        assert text_contents == kernelspec.json()
